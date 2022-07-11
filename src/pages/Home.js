9d172ed682e19c471378d2b2a60b65d6b96847d9
Paolo Siroko <86466420+paolosiroko@users.noreply.github.com>
2022-07-11 22:40:20 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "../components/styles/Home.css";
 
 export default function Home() {
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Django", "React Js"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Paolo Siroko</h2>
+        <h2 style={{ margin: "60px" }} > {`Hi, My Name is Paolo Siroko`}<span><br/>{text}</span></h2>
         <div className="prompt" style={{ margin: "40px" }}>
           <p>
             A software developer with 2 years experience and a passion for
